7263fd13e045ffd962c3794d4da2ebdd1ac97480
asimkhan8107 <110652576+asimkhan8107@users.noreply.github.com>
2023-05-17 12:55:57 +0530

    Update and rename dut.v to xor_gate.v

--- hdl/dut.v
+++
@@ -1,6 +0,0 @@
-module dut(input wire a,
-	input wire b,
-	output wire y
-);
-assign y=a^b;
-endmodule
---
+++ hdl/xor_gate.v
@@ -0,0 +1,6 @@
+module xor_gate(input wire a,
+	input wire b,
+	output wire y
+);
+assign y=a^b;
+endmodule: xor_gate
