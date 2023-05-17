b854854a7cb6ee7822dfd08af78378a0e56dd70c
asimkhan8107 <110652576+asimkhan8107@users.noreply.github.com>
2023-05-17 13:34:22 +0530

    Update xor_test.v

--- tests/wrappers/xor_test.v
+++ tests/wrappers/xor_test.v
@@ -1,14 +1,14 @@
-module xor_test(input wire a,
+module dut_test(input wire a,
                 input wire b,
                 output wire y);
           
-  xor_gate xor_gate(.a(a), 
-                    .b(b),
-                    .y(y));
+  dut dut(.a(a), 
+          .b(b),
+          .y(y));
   initial
     begin
       $dumpfile("xor.vcd");
       $dumpvars;
     end
-endmodule: xor_test
+endmodule: dut_test
       
