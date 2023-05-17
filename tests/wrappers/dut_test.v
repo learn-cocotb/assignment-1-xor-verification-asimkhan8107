452e6ca3292f534eaaf7bc91e2dcd93af9d9018e
asimkhan8107 <110652576+asimkhan8107@users.noreply.github.com>
2023-05-17 14:36:37 +0530

    Update dut_test.v

--- tests/wrappers/dut_test.v
+++ tests/wrappers/dut_test.v
@@ -2,12 +2,12 @@ module dut_test(input wire a,
                 input wire b,
                 output wire y);
           
-  dut dut(.a(a), 
-          .b(b),
-          .y(y));
+       dut dut(.a(a), 
+               .b(b),
+               .y(y));
   initial
     begin
-      $dumpfile("waveform.vcd");
+      $dumpfile("xor.vcd");
       $dumpvars;
     end
 endmodule: dut_test
